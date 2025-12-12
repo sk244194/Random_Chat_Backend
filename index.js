09666bb1e45bb166b8bc585e8972bf11beb17ab7
Shubham <sk244184@gmail.com>
2025-12-12 18:28:09 +0530

    Added Frontend Socket

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: '*' } });
+const io = new Server(server, { cors: { origin: 'https://random-chat-frontend.vercel.app/' } });
 
 let waitingUser = null;
 let rooms = {}; // roomID: { users: [socketId1, socketId2], firstUser: socketId }
